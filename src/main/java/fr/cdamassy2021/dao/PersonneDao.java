168eb4b1f822d93225516a672b6c1626cdfcef4f
Michel Plasse <mplasse@free.fr>
2021-12-14 15:09:30 +0100

    Commit initial

---
+++ src/main/java/fr/cdamassy2021/dao/PersonneDao.java
@@ -0,0 +1,70 @@
+package fr.cdamassy2021.dao;
+
+import fr.cdamassy2021.model.Personne;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author michel
+ */
+public class PersonneDao {
+
+  /**
+   * Personne de login et password donnés. Renvoie null si pas trouvé.
+   *
+   * @param login
+   * @param password
+   * @return
+   */
+  public static Personne getByLoginPassword(String login, String password) throws SQLException {
+    Personne result = null;
+    Connection connection = Database.getConnection();
+    String sql = "SELECT * FROM personne WHERE email=? AND pwd=?";
+    PreparedStatement stmt = connection.prepareStatement(sql);
+    stmt.setString(1, login);
+    stmt.setString(2, password);
+    ResultSet rs = stmt.executeQuery();
+    if (rs.next()) {
+      result = new Personne(
+              rs.getInt("id_personne"),
+              rs.getString("prenom"),
+              rs.getString("nom"),
+              rs.getString("email"),
+              rs.getString("tel"),
+              null); // pas de mot de passe en mémoire
+    }
+    return result;
+  }
+
+  /**
+   * Insere personne dans la base et positionne personne.id à la valeur
+   * auto-incrémentée dans la base.
+   *
+   * @param personne
+   * @throws SQLException
+   */
+  public void insert(Personne personne) throws SQLException {
+    Connection connection = Database.getConnection();
+    String insert = "INSERT INTO personne (prenom, nom, email, pwd, tel, inscrit_a) VALUES ( ?, ?, ?, ?,?, NOW());";
+    //compile la requete
+    PreparedStatement stmt = connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+
+    stmt.setString(1, personne.getPrenom());
+    stmt.setString(2, personne.getNom());
+    stmt.setString(3, personne.getEmail());
+    stmt.setString(4, personne.getPwd());
+    stmt.setString(5, personne.getTel());
+
+    stmt.execute();
+    // Récupérer le id auto-incrémenté
+    ResultSet rs = stmt.getGeneratedKeys();
+    if (rs.next()) {
+      // Le id est dans la 1ere colonne trouvee
+      personne.setId(rs.getInt(1));
+    }
+  }
+}
