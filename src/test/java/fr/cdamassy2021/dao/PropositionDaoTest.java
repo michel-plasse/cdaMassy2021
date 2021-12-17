fb52f5e9e3cb2f128f193f6080de5380944e9e93
kamal9401 <kamalknina@hotmail.fr>
2021-12-17 18:11:56 +0100

    Create PropositionDaoTest.java

---
+++ src/test/java/fr/cdamassy2021/dao/PropositionDaoTest.java
@@ -0,0 +1,37 @@
+package fr.cdamassy2021.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import fr.cdamassy2021.model.Proposition;
+import fr.cdamassy2021.model.Question;
+
+class PropositionDaoTest extends Cdamassy2021Test{
+
+	@Test
+	void testInsert() throws SQLException {
+		System.out.println("try insert");
+		//given:
+		PropositionDao instance  = new PropositionDao();
+
+		String enonceProposition
+		= "dites-moi si vous me trouvez dans la"
+				+ "database après le test d'insertion";
+		int idTest = -1;
+		int idQuestion = 1;
+		Proposition inserted= new Proposition(idQuestion, null, enonceProposition);
+		boolean expResult = true;
+		//when:
+		boolean result = instance.insert(inserted);
+		//then:
+		assertEquals(16, inserted.getIdProposition());
+		assertEquals(expResult, result);
+
+
+
+	}
+
+}
