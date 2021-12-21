89ad37acb8f89d7459f478559e465f06685c7f87
Ikescd <ikescd@gmail.com>
2021-12-21 15:06:25 +0100

    ajoute EFGDao dans package dao et EFG dans model

---
+++ src/main/java/fr/cdamassy2021/dao/EFGDao.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.cdamassy2021.dao;
+
+import fr.cdamassy2021.model.EFG;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author scdel
+ */
+public class EFGDao implements Dao<EFG> {
+
+    @Override
+    public boolean insert(EFG inserted) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(EFG deleted) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public EFG findById(long id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<EFG> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
