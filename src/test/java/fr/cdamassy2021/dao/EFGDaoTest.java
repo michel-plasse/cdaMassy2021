69caa0353285034f8769d61e6936e0075983d5af
Sarah Delmas <54718095+Ikescd@users.noreply.github.com>
2022-01-02 17:41:42 +0100

    ajoute le test findAllByCanal

--- src/test/java/fr/cdamassy2021/dao/EFGDaoTest.java
+++ src/test/java/fr/cdamassy2021/dao/EFGDaoTest.java
@@ -22,12 +22,13 @@ public class EFGDaoTest {
 
     public EFGDaoTest() {
     }
-
+    
+    
     protected static final String TESTINSERT_EFG
-            = "SELECT * FROM efg WHERE "
-            + "id_createur = 1 AND "
-            + "intitule = 'Enoncé' AND "
-            + "id_canal = 1";
+        = "SELECT * FROM efg WHERE "
+        + "id_createur = 1 AND "
+        + "intitule = 'Enoncé' AND "
+        + "id_canal = 1";
 
     /**
      * Réinitialisation de la base, pour maintenir la cohérence des tests malgré
@@ -97,5 +98,20 @@ public class EFGDaoTest {
         lesGroupes.add(groupeDeux);
         EFG expected = new EFG(2, 1, 1, "TP cadrage", lesGroupes);
         assertEquals(expected, result);
+    }
+
+    /**
+     * Test de la méthode findAllByCanal on vérifie que le nombre d'EFG
+     * retourné est celui attendu
+     *
+     * @throws SQLException
+     */
+    @Test
+    public void testFindAllByCanal() throws SQLException {
+        EFGDao instance = new EFGDao();
+        ArrayList<EFG> efg = instance.findAllByCanal(2);
+        int expected = 1;
+
+        assertEquals(expected, efg.size());
     }
 }
