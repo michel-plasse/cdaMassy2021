8690721e7f90bd7306317e1f4f994179e87f354c
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-02-08 14:24:15 +0100

    Update persistence with user and pwd

--- OLD/src/main/db/create-user.sql
+++ OLD/src/main/db/create-user.sql
@@ -1,4 +1,6 @@
-CREATE USER IF NOT EXISTS cdamassy2021_user@localhost IDENTIFIED BY 'cdamassy2021_pwd';
+use cdamassy2021;
+
+CREATE USER cdamassy2021_user@localhost IDENTIFIED BY 'cdamassy2021_pwd';
 GRANT ALL ON cdamassy2021.* TO cdamassy2021_user@localhost;
 GRANT EXECUTE ON cdamassy2021.* TO cdamassy2021_user@localhost;
 -- si l'instruction précédente échoue, lancer :
