38a8ee1ac18cad66425a477ec332908d5c1d7a10
kamal9401 <kamalknina@hotmail.fr>
2022-02-09 15:38:57 +0100

    personne.java
    create personne entity

--- src/fr/cdamassy2021/controller/QuestionController.java
+++ src/fr/cdamassy2021/controller/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import fr.cdamassy2021.entity.Question;
+import fr.cdamassy2021.service.QuestionService;
 
 
 /**
