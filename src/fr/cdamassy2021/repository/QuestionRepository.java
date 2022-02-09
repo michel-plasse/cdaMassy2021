9e6eb805d2b266ad91946567d1e384d99573d79b
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-02-09 18:15:14 +0100

    Update QuestionRepository.java

--- src/fr/cdamassy2021/repository/QuestionRepository.java
+++ src/fr/cdamassy2021/repository/QuestionRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import fr.cdamassy2021.entity.Question;
 
-
+@Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
 	// how to search for creator then?
 	@Query(value = "SELECT q FROM Question q WHERE q.libelle LIKE '%' || :keyword || '%'")
