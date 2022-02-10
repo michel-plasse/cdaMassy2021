3ed587ec07e8d66e72124313dcbc43f8289d1dde
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-02-10 15:49:30 +0100

    Implemente connexion utilisateur

--- src/fr/cdamassy2021/controller/HomeController.java
+++ src/fr/cdamassy2021/controller/HomeController.java
@@ -2,16 +2,52 @@ package fr.cdamassy2021.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.cdamassy2021.entity.Personne;
+import fr.cdamassy2021.service.PersonneService;
+
 
 @Controller
 public class HomeController {
+	@Autowired
+	private PersonneService personneService;
+	
 	@RequestMapping("/")
 	public ModelAndView home() {
 		ModelAndView mav = new ModelAndView("index");
 		return mav;
+	}
+
+	@RequestMapping("/connexion")
+	public ModelAndView connexion() {
+		ModelAndView mav = new ModelAndView("connexion");
+		return mav;
+	}
+	@RequestMapping("/tryconnection")
+	public ModelAndView tryConnexion(HttpServletRequest request,
+			HttpServletResponse response) {
+		String email=request.getParameter("login");  
+		String password=request.getParameter("pwd");
+		String message;
+		Personne currentUser = personneService.exist(email,password);
+		if(currentUser!=null){
+			message = "Welcome " + email + ".";
+			ModelAndView mav = new ModelAndView("welcome", 
+					"message", message);
+			mav.addObject("currentUser", currentUser);
+			return mav;  
+
+		}else{
+			message = "Wrong username or password.";
+			return new ModelAndView("errorPage", 
+					"message", message);
+		}
 	}
 }
--- src/fr/cdamassy2021/repository/PersonneRepository.java
+++ src/fr/cdamassy2021/repository/PersonneRepository.java
@@ -1,10 +1,7 @@
 package fr.cdamassy2021.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.cdamassy2021.entity.Personne;
@@ -12,6 +9,7 @@ import fr.cdamassy2021.entity.Personne;
 @Repository
 public interface PersonneRepository extends CrudRepository<Personne, Long> {
 	
-	@Query(value = "SELECT p FROM Personne p WHERE p.nom LIKE '%' || :keyword || '%'")
-	public List<Personne> search(@Param("keyword") String keyword);
+	@Query(value = "SELECT p FROM Personne p WHERE p.email = ?1 "
+			+ " AND p.pwd = ?2 ")
+	public Personne exist(String email, String pwd);
 }
