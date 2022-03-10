b0f4157cb9d91d10321feb2d786a56aebc0c1c09
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-03-10 12:17:21 +0100

    Add Repo annotation on CanalRepo

--- src/fr/cdamassy2021/repository/CanalRepository.java
+++ src/fr/cdamassy2021/repository/CanalRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import fr.cdamassy2021.entity.Canal;
 
-
+@Repository
 public interface CanalRepository extends CrudRepository<Canal, Long> {
 
 	@Query(value = "SELECT c FROM Canal c WHERE c.nom LIKE '%' || :keyword || '%'")
