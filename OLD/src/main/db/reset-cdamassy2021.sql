54500728471d8f8d52b191fc65c89d3200fc896c
Florian Langlois <93393308+FlorianLanglois@users.noreply.github.com>
2022-03-16 16:49:50 +0100

    Update reset-cdamassy2021.sql
    Pitite correction les répartitions des groupes des efgs sont mis dans l'ordre décroissant.

--- OLD/src/main/db/reset-cdamassy2021.sql
+++ OLD/src/main/db/reset-cdamassy2021.sql
@@ -50,7 +50,7 @@ BEGIN
 		-- Tableaux ne dépendant que du 1er groupe
 		INSERT INTO	efg(id_efg, intitule, date_creation, id_createur, id_canal, groupes) VALUES
     -- 2 dans le canal 1 + 1 dans le canal 2, tous du formateur 1
-			(1, 'TP définir objectif', date_effet - INTERVAL 1 MONTH, 1, 1,'2,3'),
+			(1, 'TP définir objectif', date_effet - INTERVAL 1 MONTH, 1, 1,'3,2'),
 			(2, 'TP cadrage', date_effet - INTERVAL 1 MONTH, 1, 1, '2,2'),
 			(3, "TP tests d'acceptation", date_effet - INTERVAL 1 MONTH, 1, 2, '3,2,2');
 		
