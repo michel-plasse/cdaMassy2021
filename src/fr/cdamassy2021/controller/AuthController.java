ed3dfe392e011efff472ff70dd65e5e2fe1a3a03
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-04-03 01:00:04 +0200

    Bugfix: Allow Cross Origins

--- src/fr/cdamassy2021/controller/AuthController.java
+++ src/fr/cdamassy2021/controller/AuthController.java
@@ -2,6 +2,7 @@ package fr.cdamassy2021.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import fr.cdamassy2021.service.JWTService;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthController {
 	@Autowired
 	private AuthService authService;
